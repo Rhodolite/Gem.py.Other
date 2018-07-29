06302a433bfcec5bee1b3149c50f4d1aae6a5b16
Joy Diamond <python.gem@gmail.com>
2018-07-29 13:24:38 -0400

    joy-transport-1: Squash of 6 commits
        Use `transport`
        Use `transport` more
        Use `transport` more
        Use `transport` more
        Use `transport` more
        Use `transport` more

--- CodeGenerator/Core.py
+++ CodeGenerator/Core.py
@@ -3,37 +3,13 @@
 #
 @module('CodeGenerator.Core')
 def module():
-    require_module('Capital.Ascii')
-    require_module('Capital.ContextManager')
-    require_module('Capital.DelayedFileOutput')
-    require_module('Capital.Exception')
-    require_module('Capital.Path')
-    require_module('Capital.SimpleStringIO')
-    require_module('Capital.System')
-    require_module('Capital.Traceback')
-
-
-    from Capital import create_DelayedFileOutput, create_SimpleStringOutput, empty_context_manager, execute
-    from Capital import lookup_ascii, module_path, path_join, print_exception_chain, program_exit, slice_all
-
-
-    share(
-        #
-        #   Imported functions
-        #
-        'create_DelayedFileOutput',     create_DelayedFileOutput,
-        'create_SimpleStringOutput',    create_SimpleStringOutput,
-        'empty_context_manager',        empty_context_manager,
-        'execute',                      execute,
-        'lookup_ascii',                 lookup_ascii,
-        'path_join',                    path_join,
-        'print_exception_chain',        print_exception_chain,
-        'program_exit',                 program_exit,
-
-
-        #
-        #   Imported values
-        #
-        'module_path',                  module_path,
-        'slice_all',                    slice_all,
-    )
+    transport('Capital.Ascii',                      'lookup_ascii')
+    transport('Capital.ContextManager',             'empty_context_manager')
+    transport('Capital.Core',                       'execute')
+    transport('Capital.DelayedFileOutput',          'create_DelayedFileOutput')
+    transport('Capital.Path',                       'path_join')
+    transport('Capital.SimpleStringIO',             'create_SimpleStringOutput')
+    transport('Capital.System',                     'module_path')
+    transport('Capital.System',                     'program_exit')
+    transport('Capital.System',                     'slice_all')
+    transport('Capital.Traceback',                  'print_exception_chain')
--- LockFree/Core.py
+++ LockFree/Core.py
@@ -3,26 +3,22 @@
 #
 @module('LockFree.Core')
 def module():
-    require_module('Capital.Cache')
-    require_module('Capital.Exception')
     require_module('Capital.Sleep')
     require_module('Capital.System')
     require_module('Capital.Thread')
     require_module('Capital.Traceback')
 
 
+    transport('Capital.Traceback',                  'print_exception_chain')
+
+
     from Capital import allocate_lock, change_check_interval, fetch_check_interval
-    from Capital import print_exception_chain, produce_conjure_dual, produce_conjure_triple, produce_conjure_triple__312
     from Capital import sleep, start_new_thread, thread_identifier
 
 
     share(
         'change_check_interval',            change_check_interval,
         'fetch_check_interval',             fetch_check_interval,
-        'print_exception_chain',            print_exception_chain,
-        'produce_conjure_dual',             produce_conjure_dual,
-        'produce_conjure_triple',           produce_conjure_triple,
-        'produce_conjure_triple__312',      produce_conjure_triple__312,
         'sleep',                            sleep,
         'allocate_lock',                    allocate_lock,
         'start_new_thread',                 start_new_thread,
