d46552bd5fbb3fe21a37cca9a59867e95ee1f39a
Joy Diamond <python.gem@gmail.com>
2018-07-30 15:37:37 -0400

    joy-tremolite-4: Change whitespace

--- Mothballed/Rubber/Core.py
+++ Mothballed/Rubber/Core.py
@@ -6,5 +6,5 @@ def module():
     transport('Capital.Core',                       'line')
     transport('Capital.Core',                       'privileged')
 
-    
+
     require_module('Rubber.ElasticSearch')                              #   Must appear after transport of `privileged`
